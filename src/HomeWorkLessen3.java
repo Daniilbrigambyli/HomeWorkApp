7c95fb3d882b3e7aac02b2a3c45ea7ef95476787
Daniil <brigambyli@yandex.by>
2021-10-16 20:20:45 +0300

    lessen 3.1

--- src/HomeWorkLessen3.java
+++ src/HomeWorkLessen3.java
@@ -7,18 +7,24 @@ public class HomeWorkLessen3 {
         int[] replacing = new int[]{1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1,};
         replacingNumbers(replacing);
         System.out.println();
+
         int[] numbers100 = new int[100];
         create100Numbers(numbers100);
         System.out.println();
+
         int[] multiplication = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         multiplicationNumbers(multiplication);
         System.out.println();
+
         int[][] diagonal = new int[7][7];
         diagonalNumbers(diagonal);
+
         int[] array = lenInitialValue(5, 10);
         System.out.println(Arrays.toString(array));
+
         maxMinMeaning();
         System.out.println();
+
         int[] trueFalse = {1, 2, 3, 4, 5, 5};
         System.out.println(trueOrFalse(trueFalse));
 
@@ -52,7 +58,7 @@ public class HomeWorkLessen3 {
     public static void diagonalNumbers(int[][] diagonal) {
         for (int i = 0; i < diagonal.length; i++) {
             for (int j = 0; j < diagonal[i].length; j++) {
-                if (diagonal[i] == diagonal[j] || i + j == diagonal[i].length - 1) {
+                if (i == j || i + j == diagonal[i].length - 1) {
                     diagonal[i][j] = 1;
                 }
                 System.out.print(diagonal[i][j] + " ");
@@ -63,9 +69,7 @@ public class HomeWorkLessen3 {
 
     public static int[] lenInitialValue(int len, int initialValue) {
         int[] array = new int[len];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = initialValue;
-        }
+        Arrays.fill(array, initialValue);
         return array;
     }
 
@@ -73,30 +77,27 @@ public class HomeWorkLessen3 {
         int[] maxMin = {1, 5, 3, 4, 5, 0, 7, 16};
         int max = maxMin[0];
         int min = maxMin[0];
-        for (int i = 0; i < maxMin.length; i++) {
-            if (maxMin[i] > max) {
-                max = maxMin[i];
+        for (int i = 1;  i < maxMin.length; i++) {
+            int k = maxMin[i];
+            if (k > max) {
+                max = k;
+            } if (min > k) {
+                min = k;
             }
         }
-        System.out.print(max + " ");
-        for (int i = 0; i < maxMin.length; i++) {
-            if (min > maxMin[i]) {
-                min = maxMin[i];
-            }
-        }
-        System.out.print(min);
+        System.out.print(max + " " + min);
     }
 
     public static boolean trueOrFalse(int[] trueFalse) {
         int sumLeft = 0;
         int sumRight = 0;
         int sum = 0;
-        for(int i = 0; i < trueFalse.length; i++) {
-            sum = sum + trueFalse[i];
+        for (int k : trueFalse) {
+            sum = sum + k;
         }
-        for (int i = 0; i < trueFalse.length; i++) {
-            if ((sumLeft = sumLeft + trueFalse[i]) == (sumRight = sum - trueFalse[i])) {
-            return true;
+        for (int j : trueFalse) {
+            if ((sumLeft = sumLeft + j) == (sumRight = sum - j)) {
+                return true;
             }
         }
         return false;
